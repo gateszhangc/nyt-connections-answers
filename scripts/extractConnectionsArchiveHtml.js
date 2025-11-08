3a7824df4d20de611172081c43062eee207e71a3
gateszhangc <gateszhang92@gmail.com>
2025-11-08 15:07:11 +0800

    Add new pages: connections-archive and letter-boxed-answers-today

--- scripts/extractConnectionsArchiveHtml.js
+++ scripts/extractConnectionsArchiveHtml.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const html = fs.readFileSync('connections-archive.html', 'utf8');
+const html = fs.readFileSync('../connections-archive.html', 'utf8');
 
 // Extract head content
 const headMatch = html.match(/<head>([\s\S]*?)<\/head>/i);
@@ -12,8 +12,8 @@ const bodyMatch = html.match(/<body[^>]*>([\s\S]*?)<\/body>/i);
 const body = bodyMatch ? bodyMatch[1].trim() : '';
 
 // Write to data directory
-fs.writeFileSync(path.join('data', 'connections-archive-head.html'), head);
-fs.writeFileSync(path.join('data', 'connections-archive-body.html'), body);
+fs.writeFileSync(path.join('../data', 'connections-archive-head.html'), head);
+fs.writeFileSync(path.join('../data', 'connections-archive-body.html'), body);
 
 console.log('Successfully extracted connections-archive head and body content');
 console.log('Head length:', head.length);
